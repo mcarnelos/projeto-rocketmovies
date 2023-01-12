943dea802e3b5584d34f7c7698b3e11d44cf19e6
Murilo Carnelos <muriloc.c@hotmail.com>
2023-01-11 21:03:47 -0300

    page SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -9,13 +9,14 @@ import theme from './styles/theme'
 /* import { MoviePreview } from './pages/MoviePreview' */
 /* import { CreateMovie } from './pages/CreateMovie' */
 /* import { Profile } from './pages/Profile' */
-import { SignIn } from './pages/SignIn'
+/* import { SignIn } from './pages/SignIn' */
+import { SignUp } from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> {/* compartilhando o theme com toda a aplicação */}
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
